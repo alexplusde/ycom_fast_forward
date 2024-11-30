ae0e3b4d07c9357a75ab35a51be52fe5acfce2ad
Alexander Walther <mail@alexplus.de>
2024-11-30 21:16:29 +0100

    Inhalte aus install.php und boot.php wieder korrekt verteilt

--- boot.php
+++ boot.php
@@ -13,27 +13,3 @@ if (rex::isBackend()) {
         });
     }
 }
-
-$module_id = rex_sql::factory()->setQuery('SELECT id FROM ' . rex::getTablePrefix() . 'module WHERE `key` = "ycom_fast_forward"')->getValue('id');
-
-/* Modul-ID als Addon-Property speichern */
-$addon = rex_addon::get('ycom_fast_forward');
-$addon->setProperty('module_id', $module_id);
-
-/*
-rex_category_service::addCategory(0, [
-    'catname' => 'Login',
-    'catpriority' => 1,
-    'templateId' => null,
-    'name' => null,
-    'status' => 1,
-]);
-*/
-// rex_article_content prüfen, ob slices vorhanden sind
-
-
-
-/* REX_EXTENSION_POINT nutzen, um eben erstllte Kategorie zu finden und Artikel anzulegen */
-
-rex_extension::register('CAT_ADDED', [YComFastForward::class, "CatAdded"]);
-rex_extension::register('ART_ADDED', [YComFastForward::class, "ArtAdded"]);
--- install.php
+++ install.php
@@ -1,15 +1,18 @@
 <?php
 
-// Wenn Modul mit key "ycom_fast_forward" nicht existiert, dann Modul installieren
 
 use Alexplusde\YComFastForward\YComFastForward;
 
-$input = rex_file::get(rex_path::addon('ycom_fast_forward', 'install/module/input.php'));
-$output = rex_file::get(rex_path::addon('ycom_fast_forward', 'install/module/output.php'));
+$addon = rex_addon::get('ycom_fast_forward');
 
+// Wenn Modul mit key "ycom_fast_forward" nicht existiert, dann Modul installieren
 $module = rex_sql::factory()->setQuery('SELECT id FROM ' . rex::getTablePrefix() . 'module WHERE `key` = "ycom_fast_forward"');
 
 if($module->getRows() === 0) {
+
+    $input = rex_file::get(rex_path::addon('ycom_fast_forward', 'install/module/input.php'));
+    $output = rex_file::get(rex_path::addon('ycom_fast_forward', 'install/module/output.php'));
+
     /* Modul anlegen */
     rex_sql::factory()
     ->setTable(rex::getTablePrefix() . 'module')
@@ -30,7 +33,10 @@ if($module->getRows() === 0) {
     ->update();
 }
 
-if(YComFastForward::getConfig('first_install') !== '0000-00-00 00:00:00') {
+/* Falls erste Installation, dann Datum speichern */
+if(YComFastForward::getConfig('first_install') === '0000-00-00 00:00:00') {
+    YComFastForward::setConfig('first_install', date('Y-m-d H:i:s'));
+} else {
     YComFastForward::setConfig('first_install', date('Y-m-d H:i:s'));
     return;
 }
@@ -51,6 +57,43 @@ Benötigte Kategorien / Artikel für YCom Fast Forward:
 * Logout
 */
 
+/* Extension-Points CAT_ADDED und ART_ADDED nutzen, um eben erstllte Kategorie zu finden und weitere Artikel / Slices anzulegen */
+
+rex_extension::register('CAT_ADDED', [YComFastForward::class, "CatAdded"]);
+rex_extension::register('ART_ADDED', [YComFastForward::class, "ArtAdded"]);
+
+$module_id = rex_sql::factory()->setQuery('SELECT id FROM ' . rex::getTablePrefix() . 'module WHERE `key` = "ycom_fast_forward"')->getValue('id');
+
+/* Modul-ID als Addon-Property speichern */
+$addon->setProperty('module_id', $module_id);
+
+
+// TODO: Attribut ycom_auth_type = `2` für nicht eingeloggte Nutzer
+rex_category_service::addCategory(0, [
+    'catname' => 'Login',
+    'catpriority' => 1,
+    'templateId' => null,
+    'name' => null,
+    'status' => 1,
+]);
+
+// TODO: Attribut ycom_auth_type = `1` für eingeloggte Nutzer
+rex_category_service::addCategory(0, [
+    'catname' => 'Mein Profil',
+    'catpriority' => 2,
+    'templateId' => null,
+    'name' => 'Übersicht',
+    'status' => 1,
+]);
+
+// TODO: Attribut ycom_auth_type = `1` für eingeloggte Nutzer
+rex_category_service::addCategory(0, [
+    'catname' => 'Logout',
+    'catpriority' => 3,
+    'templateId' => null,
+    'name' => 'Logout',
+    'status' => 1,
+]);
 
 // YCom-Nutzer anlegen
 if(rex_ycom_user::query()->where('email', 'mail@example.org')->findOne() === null) {
