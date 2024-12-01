66ee6cd433e9060b90ca242569c969263e803337
Alexander Walther <mail@alexplus.de>
2024-12-01 02:30:35 +0100

    Create health.php

---
+++ pages/health.php
@@ -0,0 +1,7 @@
+<?php
+
+// To-Do: Status-Bericht über YCom-Benutzer-Datenbank
+// 1. Anzahl der Benutzer
+// 2. Anzahl der Benutzer je Status (aktiv, inaktiv, gesperrt)
+// 3. Anzahl der Benutzer je Rolle / YCom-Gruppe (z.B. Administrator, Redakteur, Autor, Gast)
+// 4. Anzahl der Benutzer, die Nutzungsbedingungen akzeptiert haben
